1d1e5eb451c91682c176c5015aab1896d3c8345f
cedcoss <you@example.com>
2022-04-07 19:41:05 +0530

    fourth task commit

--- index.js
+++ index.js
@@ -9,40 +9,51 @@
 
 var empcart=[];
 class Employee{
-    empclass(){
-        var empid=document.getElementById("empid").value;
-        var empname=document.getElementById("empname").value;
-        var empsal = parseInt(document.getElementById("empsal").value);
-        var cart={};
-        cart.perid=document.getElementById("empid").value;
-        cart.pername=document.getElementById("empname").value;
-        cart.persal=document.getElementById("empsal").value;
-        empcart.push(cart);
-        var myjson=JSON.stringify(empcart);
+    // empclass(){
+    //     // var empid=document.getElementById("empid").value;
+    //     // var empname=document.getElementById("empname").value;
+    //     // var empsal = parseInt(document.getElementById("empsal").value);
+        constructor(id,name,basicsalary){
+            this.id=id;
+            this.name=name;
+            this.basicsalary=basicsalary;
+
+        }
+        // var cart={};
+        // cart.perid=document.getElementById("empid").value;
+        // cart.pername=document.getElementById("empname").value;
+        // cart.persal=document.getElementById("empsal").value;
+        // empcart.push(cart);
+        // var myjson=JSON.stringify(empcart);
+        display(){
+            var html2="";
+
+                html2+="<tr><td>"+this.id+"</td><td>"+this.name+"</td><td>"+this.basicsalary+"</td><td class='del'>Delete</td></tr>"
+               
+    
+    
+    
+            ; document.getElementById("output").innerHTML=html+html2+html1;
+             
+    
+    
+        }
 
     
 
 
     }
-    display(){
-        var html2="";
-        empcart.forEach(element=>{
-            html2+="<tr><td>"+ element.perid + "</td><td>"+element.pername+"</td><td>"+element.persal+"</td><td class='del'>Delete</td></tr>"
-           
+    
 
 
-
-        });document.getElementById("output").innerHTML=html+html2+html1;
-         
-
-
-    }
-}
-
-    var empl= new Employee();
     function Addclass(){
-        empl.empclass();
-        empl.display();
+        // empl.empclass();
+        // empl.display();
+        var empid=document.getElementById("empid").value;
+     var empname=document.getElementById("empname").value;
+    var empsal = parseInt(document.getElementById("empsal").value);
+    var empl=new Employee(empid,empname,empsal);
+    empl.display()
     }
    
 
