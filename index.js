7f8bfacb6a53c7b049d8f4f1370bd8f0d031851b
cedcoss <you@example.com>
2022-04-07 18:32:18 +0530

    second task commit

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
-var empcart=[];
-function Add(){
-    var empid=document.getElementById("empid").value;
-    var empname=document.getElementById("empname").value;
-    var empsal = parseInt(document.getElementById("empsal").value);
-    var cart={};
-    cart.perid=document.getElementById("empid").value;
-    cart.pername=document.getElementById("empname").value;
-    cart.persal=document.getElementById("empsal").value;
-    empcart.push(cart);
+
+   
+   
 
     var html="<table><tr><th>ID</th><th>Name</th><th>Total Salary</th><th>Action</th></tr>"
     var html1="</table>";
-    empcart.forEach(element=>{
-        html+="<tr><td>"+ element.perid + "</td><td>"+element.pername+"</td><td>"+element.persal+"</td></tr>"
-    });
-    document.getElementById("output").innerHTML=html+html1;
+    
 
+
+var empcart=[];
+class Employee{
+    empclass(){
+        var empid=document.getElementById("empid").value;
+        var empname=document.getElementById("empname").value;
+        var empsal = parseInt(document.getElementById("empsal").value);
+        var cart={};
+        cart.perid=document.getElementById("empid").value;
+        cart.pername=document.getElementById("empname").value;
+        cart.persal=document.getElementById("empsal").value;
+        empcart.push(cart);
+
+    
+
+
+    }
+    display(){
+        var html2="";
+        empcart.forEach(element=>{
+            html2+="<tr><td>"+ element.perid + "</td><td>"+element.pername+"</td><td>"+element.persal+"</td><td class='del'>Delete</td></tr>"
+           
+
+
+
+        });document.getElementById("output").innerHTML=html+html2+html1;
+         
+
+
+    }
 }
+
+    var empl= new Employee();
+    function Addclass(){
+        empl.empclass();
+        empl.display();
+    }
+   
+
+
 
