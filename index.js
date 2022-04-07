fadc6dce4d7aef5ce4a08135f8f8a0a2f7650334
cedcoss <you@example.com>
2022-04-07 20:02:34 +0530

    fifth task commit

--- index.js
+++ index.js
@@ -2,38 +2,50 @@
    
    
 
-    var html="<table><tr><th>ID</th><th>Name</th><th>Total Salary</th><th>Action</th></tr>"
+    var html="<table><tr><th>ID</th><th>Name</th><th>Total Salary</th></tr>"
     var html1="</table>";
     
 
 
 var empcart=[];
 class Employee{
-    // empclass(){
-    //     // var empid=document.getElementById("empid").value;
-    //     // var empname=document.getElementById("empname").value;
-    //     // var empsal = parseInt(document.getElementById("empsal").value);
-        constructor(id,name,basicsalary){
-            this.id=id;
-            this.name=name;
-            this.basicsalary=basicsalary;
+    empclass(){
+         var empid=document.getElementById("empid").value;
+         var empname=document.getElementById("empname").value;
+         var empsal = parseInt(document.getElementById("empsal").value);
+        // constructor(id,name,basicsalary){
+        //     this.id=id;
+        //     this.name=name;
+        //     this.basicsalary=basicsalary;
 
-        }
-        // var cart={};
-        // cart.perid=document.getElementById("empid").value;
-        // cart.pername=document.getElementById("empname").value;
-        // cart.persal=document.getElementById("empsal").value;
-        // empcart.push(cart);
+        // }
+        var cart={};
+        cart.perid=document.getElementById("empid").value;
+        cart.pername=document.getElementById("empname").value;
+        cart.persal=document.getElementById("empsal").value;
+        empcart.push(cart);
+    }
         // var myjson=JSON.stringify(empcart);
         display(){
-            var html2="";
+            // var html2="";
 
-                html2+="<tr><td>"+this.id+"</td><td>"+this.name+"</td><td>"+this.basicsalary+"</td><td class='del'>Delete</td></tr>"
+            //     html2+="<tr><td>"+this.id+"</td><td>"+this.name+"</td><td>"+this.basicsalary+"</td><td class='del'>Delete</td></tr>"
                
     
     
     
-            ; document.getElementById("output").innerHTML=html+html2+html1;
+            // ; document.getElementById("output").innerHTML=html+html2+html1;
+            try{
+                var html2="";
+                empcart.forEach((element) => {
+                    var mul=element.persal*1.10;
+                    html2+="<tr><td>"+element.perid+"</td><td>"+element.pername+"</td><td>"+element.persal+"</td><td>"+mul+"</td></tr>"
+                });
+                document.getElementById("output").innerHTML=html+html2+html1;
+
+            } catch (error){
+                console.log(error)
+            }
              
     
     
@@ -49,12 +61,13 @@ class Employee{
     function Addclass(){
         // empl.empclass();
         // empl.display();
-        var empid=document.getElementById("empid").value;
-     var empname=document.getElementById("empname").value;
-    var empsal = parseInt(document.getElementById("empsal").value);
-    var empl=new Employee(empid,empname,empsal);
-    empl.display()
-    }
+    //     var empid=document.getElementById("empid").value;
+    //  var empname=document.getElementById("empname").value;
+    // var empsal = parseInt(document.getElementById("empsal").value);
+    var empl=new Employee();
+    empl.empclass();
+    empl.display();
+    };
    
 
 
